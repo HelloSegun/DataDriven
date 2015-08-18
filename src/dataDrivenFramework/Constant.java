724cea3d26fe6e32c9bef5634a73f3571feb5238
Kuyes10 <Kuyes10@DEV-SEGUN.hubdev.interoute.com>
2015-08-18 12:12:05 +0100

    Update 18.08.2015 12:14

---
+++ src/dataDrivenFramework/Constant.java
@@ -0,0 +1,17 @@
+package dataDrivenFramework;
+
+public class Constant {
+
+	public static final String URL = "";
+
+	public static final String Num1 = "10.25";
+
+	public static final String Num2 = "50.56";
+
+	public static final String Expected_Result = "5.1824";
+
+	public static final String Path_TestData = "/DataDriven/src/testData/percentCalculator.xlsx";
+
+	public static final String File_TestData = "percentCalculator";
+
+}
